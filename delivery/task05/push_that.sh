40d3e1c6bfa0f848b139b95f2c561fe835fa7756
Lyviroxane~ <tinanavira@gmail.com>
2024-11-28 01:36:36 +0100

    Auto commit

---
+++ delivery/task05/push_that.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the repository
+cd root/kalidelivery-
+
+# Add all changes
+git add .
+
+# Commit changes with an auto message
+git commit -m "Auto commit"
+
+# Push changes to remote repository
+git push
